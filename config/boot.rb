cff35fe9f67ccb9bf16016526a66f69ffb09f756
MicFin <mrfinneran@gmail.com>
2019-07-17 19:46:00 +0300

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-ENV['PORT'] ||= '4741'
+ENV['PORT'] ||= '3000'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
